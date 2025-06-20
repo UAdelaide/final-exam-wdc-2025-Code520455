5bb56dffefbd524ff16eb22f6ebf06d5b497bcca
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 12:37:06 +0000

    [automatic commit] 2025-06-20 12:37:06 UTC 129.127.202.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,7 +37,7 @@ router.get('/me', (req, res) => {
 
 
 router.post('/login',async(req, res) => {
-const {username, password} = req.body;
+const {user, password} = req.body;
 try{
   const[data] = await db.query(`
     SELECT user_id, username, role FROM Users WHERE username =? AND password_hash=?
