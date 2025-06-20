6347ed9a3d7598d4e8b59513eb6f89ce554f5676
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 05:46:59 +0000

    [automatic commit] 2025-06-20 05:46:58 UTC 129.127.202.41

--- part1/app.js
+++ part1/app.js
@@ -169,7 +169,7 @@ app.get('/api/walkrequests/open', async (req,res) => {
 
 app.get('/api/walkers/summary', async (req,res) => {
     try {
-        const[dogs] = await db.execute('SELECT user.username AS walk_username, COUNT(DISTINCT walk_re');
+        const[dogs] = await db.execute('SELECT user.username AS walk_username, COUNT(DISTINCT wa');
         res.json(dogs);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs' });
