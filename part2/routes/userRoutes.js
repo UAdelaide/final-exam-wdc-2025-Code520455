0dc0dcb64ef21bfa143ae92b24bb540f6b13381f
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 15:06:37 +0000

    [automatic commit] 2025-06-20 15:06:37 UTC 129.127.202.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,6 +72,8 @@ router.post('/logout',(req,res) => {
       if(err){
         console.error("Error destroying session:",err);
         return res.status(500).json({message: 'Could not log out, please try again'});
+      } else{
+        res.clearCookie('connect')
       }
     })
   }
