850ca3acbc86126179a4df541cf34e896b6d21b7
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 04:39:55 +0000

    [automatic commit] 2025-06-20 04:39:54 UTC 129.127.202.41

--- part1/app.js
+++ part1/app.js
@@ -130,7 +130,18 @@ let db;
       `);
     }
 
-    // insert 
+    // insert WalkRequests Data
+      await db.execute
+      (`
+        INSERT INTO Dogs (owner_id, name, size) VALUES
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
+        ((SELECT user_id FROM Users WHERE username = 'eveowner'), 'Milo', 'medium'),
+        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Luna', 'small');
+
+      `);
+    }
 
 
 
