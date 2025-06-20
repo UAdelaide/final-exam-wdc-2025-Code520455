4f37dc5725d0f5ff49505d0ce084718751e0d89a
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 04:38:51 +0000

    [automatic commit] 2025-06-20 04:38:50 UTC 129.127.202.41

--- part1/app.js
+++ part1/app.js
@@ -117,7 +117,18 @@ let db;
 
       `);
 
-    // insert 
+    // insert dogs data
+      await db.execute
+      (`
+        INSERT INTO Users (username, email, password_hash, role) VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('vicky', 'vicky@example.com', 'hashed321', 'walker'),
+        ('eveowner', 'eve@example.com', 'hashed654', 'owner');
+
+      `);
+
     }
 
 
