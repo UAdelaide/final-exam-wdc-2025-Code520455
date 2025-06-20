393966e3c6939b1785b563fa144a55c377e0aa30
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 13:21:22 +0000

    [automatic commit] 2025-06-20 13:21:22 UTC 129.127.202.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -47,7 +47,7 @@ router.post('/login', async (req, res) => {
     `, [username]);
 
     if (rows.length === 0) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ success: false, message: 'User not found' });
     }
     req.session.user ={
       id: rows[0].user_id,
