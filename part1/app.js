96087e9833b7b75f8653fdba4051209905849ce0
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 04:59:58 +0000

    [automatic commit] 2025-06-20 04:59:58 UTC 129.127.202.41

--- part1/app.js
+++ part1/app.js
@@ -151,7 +151,7 @@ app.get('/api/dogs', async (req,res) => {
         const[dogs] = await db.execute('SELECT * FROM Dogs');
         res.json(dogs);
     } catch (err) {
-        res.status(500).json({error: 'Failed to fetch dogs'})
+        res.status(500).json({ error: 'Failed to fetch dogs' });
     }
 });
 
