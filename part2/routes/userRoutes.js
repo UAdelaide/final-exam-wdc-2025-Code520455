9f78063ff643c58e7d90a8f6b23c0ea02a543a5a
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 12:35:42 +0000

    [automatic commit] 2025-06-20 12:35:41 UTC 129.127.202.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -41,7 +41,7 @@ const {username, password} = req.body;
 try{
   const[data] = await db.query(`
     SELECT user_id, username, role FROM Users WHERE username =? AND password_hash=?
-    `,[user,pass]);
+    `,[username,password]);
 
     if (data.length === 0){
       return res.status(401).json({error: 'Invalid Credentials'});
