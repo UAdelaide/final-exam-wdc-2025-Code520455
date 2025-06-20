e394ea6a1ebc8a872736694d190b4d533e03b2df
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 16:49:04 +0000

    [automatic commit] 2025-06-20 16:49:04 UTC 129.127.202.41

--- part2/app.js
+++ part2/app.js
@@ -9,26 +9,16 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
-// 🛠️ Session middleware (ADD THIS)
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'default-secret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    httpOnly: true,
-    secure: false, // Only true if using HTTPS
-    sameSite: 'lax'
-  }
-}));
-
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 const dogRoutes = require('./routes/dogRoutes');
 
-app.use('/api/dogs', dogRoutes);
-app.use('/', userRoutes);
+
+app.use('/api/dogs',dogRoutes);
+app.use('/',userRoutes);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+// Export the app instead of listening here
 module.exports = app;
