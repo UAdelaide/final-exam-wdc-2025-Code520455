995f5912688af518bcf5421c1db652d917165d09
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 15:52:58 +0000

    [automatic commit] 2025-06-20 15:52:57 UTC 129.127.202.41

--- part2/app.js
+++ part2/app.js
@@ -15,7 +15,7 @@ const userRoutes = require('./routes/userRoutes');
 const dogRoutes = require('./routes/dogRoutes');
 
 
-app
+app.use()
 app.use('/',userRoutes);
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
