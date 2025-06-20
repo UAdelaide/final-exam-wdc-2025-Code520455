5167cb642705477b8a2bd8bc49b7f759a9f7b42d
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 04:40:59 +0000

    [automatic commit] 2025-06-20 04:40:58 UTC 129.127.202.41

--- part1/app.js
+++ part1/app.js
@@ -133,13 +133,12 @@ let db;
     // insert WalkRequests Data
       await db.execute
       (`
-        INSERT INTO Dogs (owner_id, name, size) VALUES
-        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
-        ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Rocky', 'large'),
-        ((SELECT user_id FROM Users WHERE username = 'eveowner'), 'Milo', 'medium'),
-        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Luna', 'small');
-
+        INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+        ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Rocky'), '2025-06-11 10:00:00', 60, 'River Trail', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Milo'), '2025-06-12 14:00:00', 30, 'City Gardens', 'open'),
+        ((SELECT dog_id FROM Dogs WHERE name = 'Luna'), '2025-06-13 17:00:00', 20, 'Sunset Park', 'open');
       `);
     }
 
