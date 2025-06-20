ad8aa16ed62873d4410330d49ca2b550ef55a309
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 12:52:07 +0000

    [automatic commit] 2025-06-20 12:52:07 UTC 129.127.202.41

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -186,21 +186,20 @@ function login(){
 
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) {
+        if (this.readyState == 4 && this.status === 200) {
             alert("Welcome "+this.responseText);
         } else if (this.readyState === 4 && this.status >= 400) {
             alert("Login failed");
         }
     };
 
-    // Open connection to server & send the post data using a POST requesta
+    // Open connection to server & send the post data using a POST request
     // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/api/Users/login", true);
+    xmlhttp.open("POST", "/api/users/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
 
 }
-
 function logout(){
 
     // Create AJAX Request
