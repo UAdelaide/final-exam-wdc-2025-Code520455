9f39ad930d0f3a3ffba1158f99f4188ab705a486
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 18:17:59 +0000

    [automatic commit] 2025-06-20 18:17:58 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,26 +2,25 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-function isAuthenticated(req, res, next){
-    if(req.session && req.session.user){
+function isAuthenticated(req, res, next) {
+    if (req.session && req.session.user) {
         return next();
     }
-    res.status(401).json({ error: 'NOT AUTHORIZED : Please login'});
+    res.status(401).json({ error: 'NOT AUTHORIZED : Please login' });
 }
-router.get('/',isAuthenticated, async(req,res) => {
+router.get('/', isAuthenticated, async (req, res) => {
     const owner_id = req.session.user.id;
-    try{
-        if(!owner_id){
-            return res.status(400).json({error: 'Owner ID is not found in session'});
+    try {
+        if (!owner_id) {
+            return res.status(400).json({ error: 'Owner ID is not found in session' });
         }
-        const[data] = await db.query(
+        const [data] = await db.query(
             `SELECT name, dog_id, size FROM Dogs
             WHERE owner_id = ?
-            `,[owner_id]
-         );
+            `, [owner_id]);
         res.json(data);
-    } catch(error){
-        res.status(500).json({error: 'Fail to fetch data from dog table'});
+    } catch (error) {
+        res.status(500).json({ error: 'Fail to fetch data from dog table' });
     }
 });
 module.exports = router;
