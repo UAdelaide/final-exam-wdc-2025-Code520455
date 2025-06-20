b65a4628f11fa05a9d66be8bf6ac497d4e881544
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-20 16:02:44 +0000

    [automatic commit] 2025-06-20 16:02:43 UTC 129.127.202.41

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -28,12 +28,12 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// router.get('/me', (req, res) => {
-//   if (!req.session.user) {
-//     return res.status(401).json({ error: 'Not logged in' });
-//   }
-//   res.json(req.session.user);
-// });
+router.get('/me', (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Not logged in' });
+  }
+  res.json(req.session.user);
+});
 
 // Login button
 
