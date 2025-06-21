9ecbc3f7a720cec9917fdb069dab92285117ce0d
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-21 03:20:36 +0000

    [automatic commit] 2025-06-21 03:20:36 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-function authen(req, res, next) {
+function authorization(req, res, next) {
     if (req.session && req.session.user) {
         return next();
     }
     res.status(401).json({ error: 'NOT AUTHORIZED : Please login' });
 }
-router.get('/', isAuthenticated, async (req, res) => {
+router.get('/', authorization, async (req, res) => {
     const owner_id = req.session.user.id;
 
         if (!owner_id) {
