f521c330d51ee69626757a59fb06472995d8708e
Rahul Saha <520455@eynesbury.sa.edu.au>
2025-06-21 03:04:43 +0000

    [automatic commit] 2025-06-21 03:04:43 UTC

--- part1/app.js
+++ part1/app.js
@@ -147,7 +147,7 @@ let db;
 // Route to return dogs as JSON
 app.get('/api/dogs', async (req,res) => {
     try {
-        const[dogs] = await db.execute('SELECT d.dog_id, d.name, d.size, u.username AS owner_name FROM Dogs d JOIN Users');
+        const[dogs] = await db.execute('SELECT d.dog_id, d.name, d.size, u.username AS owner_name FROM Dogs d JOIN Users u on d.owner_id = u.user_id');
         res.json(dogs);
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs' });
